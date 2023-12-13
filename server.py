3747b5d73dca9b011a47433913cacdc130a21e3f
etherealgemini <qqq1114174867@163.com>
2023-12-13 21:46:50 +0800

    FIX: 现在能正确下载文件了，曾因缺少content-length标头导致无法正确下载

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import base64
+import chunk
 import copy
 import mimetypes
 import mimetypes as mime
@@ -6,19 +7,21 @@ import os
 from pathlib import Path
 import socket
 import threading
-
-import send2trash
-
 from util import *
 
 # 运行后，将在路径D:\\temp\pythonServer创建根目录文件夹，浏览器中运行http:\\localhost:8000\查看
 
 NEWLINE = "\r\n"
-DATA_ROOT = "D:\\\\temp\\pythonServer"
+FILE_ROOT = os.getcwd()
+DATA_ROOT = FILE_ROOT + "\\data"
 LOCATION = "http:\\\\localhost:8000\\"
 SCHEME = "http/1.1"
 MIME_TYPE = {
     "html": "text/html"
+}
+CHUNK_SIZE = 1024 * 20
+user_dict = {
+    "test": "123456"
 }
 
 
@@ -51,6 +54,11 @@ def url_decoder(url: str) -> dict[str]:
     params, r = r.split("?", 1) if "?" in r else (r, "")
     query, fragment = r.split("#", 1) if "#" in r else (r, "")
     target = path.split("/")[-1]
+    queries = query.split("&") if "&" in query else list(query)
+    queries_dict = dict()
+    for q in queries:
+        key, value = q.split("=")
+        queries_dict[key] = value
     return {
         "scheme": scheme,
         "net_location": net_location,
@@ -58,7 +66,8 @@ def url_decoder(url: str) -> dict[str]:
         "params": params,
         "query": query,
         "fragment": fragment,
-        "target": target
+        "target": target,
+        "queries_dict": queries_dict
     }
 
 
@@ -93,24 +102,77 @@ class server:
     """
 
     def handle_conn(self, client_socket: socket.socket, client_address: tuple):
+        data = client_socket.recv(4096)
+        req = data.decode("utf-8")
+        self.handle_first_req(client_socket, req)
+
         while True:
             data = client_socket.recv(4096)
             req = data.decode("utf-8")
-            response = self.handle_request(req)
-            if type(response).__name__ == "str":
-                response = response.encode('utf-8')
-            try:
-                client_socket.send(response)
-            except TypeError:
-                if response is None:
-                    continue
-                else:
-                    print(type(req))
-                    print(len(req))
-                    time.sleep(10)
+            if len(data) < 1:
+                continue
+            response = self.handle_request(client_socket, req)
+            self.send(client_socket, response)
             # finally:
-                # client_socket.close()
-                # break
+            # client_socket.close()
+            # break
+
+    @staticmethod
+    def send(client_socket: socket.socket, response):
+        if type(response).__name__ == "str":
+            print(1)
+            response = response.encode('utf-8')
+        print("send")
+        client_socket.send(response)
+
+    def handle_first_req(self, client_socket, req):
+
+        auth_flag = self.check_auth(req)
+
+        if auth_flag:
+            self.send(client_socket, self.handle_request(client_socket, req))
+            return
+
+        response = self.authorization()
+        self.send(client_socket, response)
+
+        data = client_socket.recv(4096)
+        req = data.decode("utf-8")
+        auth_flag = self.check_auth(req)
+
+        if not auth_flag:
+            self.send(client_socket, self.unAuthorized())
+            client_socket.close()
+        self.send(client_socket, self.pass_auth())
+        return
+
+    @staticmethod
+    def check_auth(req) -> bool:
+        temp = req.split('\r\n\r\n', 1)
+        headers_ = temp[0]
+        headers = headers_.split(NEWLINE)
+        auth_flag = False
+        for header in headers:
+            if not header.__contains__("Authorization"):
+                continue
+            auth_flag = True
+            _, r = header.split(":")
+            temp = r.split(" ")
+            if len(temp) < 2:
+                auth_flag = False
+                break
+            auth_method, code = temp[0], temp[len(temp) - 1]
+            temp = base64.b64decode(code).decode("utf-8").split(":")
+            print(temp)
+            if len(temp) < 2:
+                auth_flag = False
+                break
+            usr_name, pw = temp[0], temp[1]
+            if user_dict[usr_name] != pw:
+                auth_flag = False
+            auth_flag = True
+            break
+        return auth_flag
 
     """
     method URL version CRLF <- request_line
@@ -122,7 +184,7 @@ class server:
     payload
     """
 
-    def handle_request(self, req):
+    def handle_request(self, client_socket, req):
         try:
             temp = req.split('\r\n\r\n', 1)
             header = temp[0]
@@ -135,52 +197,89 @@ class server:
             url = request_line[1]
 
             decoded_url = url_decoder(url)
+            # self.session_worker(decoded_url)
         except IndexError:
-            a=1
+            a = 1
 
         if len(request_line) == 0:
             return
 
         if req_method == "GET":
-            return self.get_request(decoded_url)
+            return self.get_request(client_socket, decoded_url)
         elif req_method == "POST":
-            return self.post_request(decoded_url,body)
+            return self.post_request(client_socket, decoded_url, body)
         return self.not_supported_request()
 
-    def get_request(self, decoded_url):
+    def get_request(self, client_socket, decoded_url):
 
         if "." in decoded_url["target"] and decoded_url["target"][-1] != ".":
-            body = self.download(decoded_url)
+            body = self.download(client_socket, decoded_url)
         else:
-            body = self.view(decoded_url)
+            body = self.view(client_socket, decoded_url)
         return body
 
-    def post_request(self, decoded_url,body):
+    def post_request(self, client_socket, decoded_url, body):
 
         if decoded_url["target"] == "upload":
-            return self.upload(decoded_url,body)
+            return self.upload(decoded_url, body)
         elif decoded_url["target"] == "delete":
             return self.delete(decoded_url)
+        else:
+            return self.not_supported_request()
 
-    def download(self, decoded_url):
+    def download(self, client_socket, decoded_url):
         path = decoded_url['path']
         path = DATA_ROOT + "\\\\" + path.replace("\\", "\\\\")
         path_ = Path(path)
         print(f"download: {decoded_url['path']}")
         ftype = mimetypes.guess_type(path_)[0]
         content = path_.open('rb').read()
+        return self.download_regular(content, ftype)
 
+        # q_dict = decoded_url["queries_dict"]
+        # if q_dict is None or q_dict.get("chunk") is None or q_dict["chunked"] != 1:
+        #     return self.download_regular(content, ftype)
+        # else:
+        #     return self.send_chunked(client_socket, content, ftype)
+
+    @staticmethod
+    def download_regular(content, mime_type) -> bytes:
         resp = Response()
-        resp.set_status_line(SCHEME,200,"OK")
-        print(mimetypes.guess_type(path_)[0])
-        resp.set_content_type(ftype,"")
+        resp.set_status_line(SCHEME, 200, "OK")
+        # print(mimetypes.guess_type(path)[0])
+        resp.set_content_type(mime_type, "")
         resp.set_keep_alive()
         resp.body = content
         out = resp.build_byte()
+        # response = 'HTTP/1.1 200 OK\r\n'  # 模拟相应行
+        # response += 'Content-Type:text/html\r\n'  # 模拟响应头
+        # response += '<h1>Sorry...</h1>'  # 模拟响应体
+        # response += '\r\n'
+        # out = response.encode()
 
         return out
 
-    def view(self, decoded_url):
+    def send_chunked(self, client_socket, content, mime_type):
+        resp = Response()
+        resp.set_status_line(SCHEME, 200, "OK")
+        resp.set_content_type(mime_type, "")
+        resp.set_keep_alive()
+        resp.set_chunked()
+        resp.body = None
+
+        resp_chunk = Response()
+        pointer = 0
+        while pointer < len(content):
+            chunk_data = content[pointer:pointer + CHUNK_SIZE]
+            pointer += CHUNK_SIZE
+            resp_chunk.body = chunk_data
+            self.send(client_socket, resp_chunk.build())
+
+        raise NotImplementedError("")
+        # return
+
+    @staticmethod
+    def view(client_socket, decoded_url):
         # if decoded_url['target'].endswith("."):
         path = decoded_url['path']
         path = path.replace("\\", "\\\\")
@@ -203,11 +302,6 @@ class server:
         path_ = Path(path)
         path_.open('wb').write(body)
         pass
-        # if path_.exists():
-        #     return
-        # else:
-        #
-        # pass
 
     def delete(self, decoded_url):
         print(f"delete file at \"{decoded_url['params']}\"")
@@ -216,6 +310,37 @@ class server:
     def not_supported_request(self):
         print("request not supported")
         pass
+
+    @staticmethod
+    def authorization():
+        resp = Response()
+        resp.set_status_line(SCHEME, 401, "Unauthorized")
+        resp.set_auth()
+        resp.set_keep_alive()
+        resp.body = open("401.html", "r").read()
+        return resp.build()
+
+    @staticmethod
+    def unAuthorized():
+        resp = Response()
+        resp.set_status_line(SCHEME, 401, "Unauthorized")
+        resp.set_keep_alive(False)
+        resp.set_content_type(MIME_TYPE["html"], "utf-8")
+        resp.body = open("401.html", "r").read()
+        return resp.build()
+
+    def session_worker(self, decoded_url):
+
+        pass
+
+    def pass_auth(self):
+        resp = Response()
+        resp.set_status_line(SCHEME, 200, "OK")
+        resp.set_auth()
+        resp.set_keep_alive()
+        # resp.body = "http://localhost:8000/init"
+        resp.body = ""
+        return resp.build()
 
 
 class Response:
@@ -234,18 +359,37 @@ class Response:
     def set_content_type(self, type: str, charset: str):
         self.headers["Content-Type"] = type
         if charset != "":
-            self.headers["Content-Type"] += "; charset="+charset
+            self.headers["Content-Type"] += "; charset=" + charset
 
-    def set_keep_alive(self):
-        self.headers["Connection"] = "keep-alive"
+    def set_keep_alive(self, alive=True):
+        if alive:
+            self.headers["Connection"] = "keep-alive"
+        else:
+            self.headers["Connection"] = "close"
+
+    def set_auth(self):
+        self.headers["WWW-Authenticate"] = "Basic realm=\"Authorization Required\""
+
+    def set_content_length(self, length):
+        self.headers["Content-Length"] = str(length)
+
+    def set_chunked(self):
+        self.headers["Transfer-Encoding"] = "chunked"
 
     def remove_header(self, header: str):
         self.headers.pop(header)
 
     def build(self) -> str:
+        if self.headers.get("Transfer-Encoding") is None and self.headers.get("Content-Length") is None:
+            print("set content_length")
+            self.set_content_length(len(self.body.encode()))
+        return self._build()
+
+    def _build(self) -> str:
         st_line = self.status_line
         hds = self.headers
         bd = self.body
+        if bd is None: bd = ""
 
         msg = st_line
         msg += NEWLINE
@@ -258,10 +402,11 @@ class Response:
 
         return msg
 
-    def build_byte(self)->bytes:
+    def build_byte(self) -> bytes:
         st_line = self.status_line
-        hds = self.headers
         bd = self.body
+        self.headers["Content-Length"] = len(bd)
+        hds = self.headers
 
         msg = st_line
         msg += NEWLINE
@@ -269,9 +414,10 @@ class Response:
             msg += hd + ":" + str(hds[hd])
             msg += NEWLINE
         msg += NEWLINE
-        msg = msg.encode('utf-8')
+        msg = msg.encode()
         msg += bd
         self.message = msg
+
         return msg
 
 
